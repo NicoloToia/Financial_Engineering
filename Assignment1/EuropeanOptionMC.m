7dfcae52e063a830cd85235a65bb280432c1e9dd
jstringara <62561822+jstringara@users.noreply.github.com>
2024-02-20 18:23:50 +0100

    Refactor MonteCarlo

--- Assignment1/EuropeanOptionMC.m
+++ Assignment1/EuropeanOptionMC.m
@@ -10,18 +10,27 @@ function optionPrice=EuropeanOptionMC(F0,K,B,T,sigma,N,flag)
 % N:     number of simulations
 % flag:  1 call, -1 put
 
-% generate random numbers from a standard Gaussian distribution
+% extract N random numbers from a standard gaussian
 g = randn(N,1);
 
-priceMC = 0;
-for i = 1:N
-    priceMC = priceMC + (B/N)*max(F0*exp(-(sigma^2)*T*0.5 + sigma*sqrt(T)*g(i)) - K,0); 
-end 
+% Monte Carlo simulation (one time step)
+% Compute the value of the forward at time T for each simulation
+% Black Model: Ft = F0 * exp(-(sigma^2)*T*0.5 + sigma*sqrt(T)*g)
+Ftt = F0 * exp( -0.5 * sigma^2 * T  + sigma * sqrt(T) * g);
+
+% Compute the call option price for each simulation
+Ct = max((Ftt - K),0);
+
+% Compute the price of the call option as the mean of the simulations discounted
+C0 = mean(Ct) * B;
+
+
 if flag == 1
-    optionPrice = priceMC;
-else
-    % Call-Put parity (European)
-    optionPrice = priceMC - B*(F0 - K);
+    optionPrice = C0;
+else % put option
+    % leverage the put-call parity
+    % C0 - P0 = B*(F0 - K)
+    optionPrice = C0 - B*(F0 - K);
 end
 
 end % function EuropeanOptionMC
