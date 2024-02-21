56e5f92e304211142dbea8d3a02e2bc4a775f235
jstringara <62561822+jstringara@users.noreply.github.com>
2024-02-21 18:28:55 +0100

    Implement vega for KnockIN

---
+++ Assignment1/VegaKI.m
@@ -0,0 +1,28 @@
+function vega= VegaKI(F0,K,KI,B,T,sigma,N,flagNum)
+% Vega of a knock-in call option
+%
+% INPUT:
+% F0:       forward price
+% K:        strike price
+% KI:       knock-in barrier
+% B:        discount factor
+% T:        maturity
+% sigma:    volatility
+% N:        number of simulations for MC and number of steps for CRR
+% flagNum:  1 for CRR, 2 for MC, 3 for closed-form
+%
+% OUTPUT:
+% vega:     vega of the option
+
+switch (flagNum)
+    case 1
+        vega = VegaCRR(F0,K,B,T,sigma,N);
+    case 2
+        vega = VegaMC(F0,K,KI,B,T,sigma,N);
+    case 3
+        vega = VegaCF(F0,K,KI,B,T,sigma);
+    otherwise
+        error('VegaKI: invalid flagNum');
+end
+
+end
