dd5f42e8cce872cf807dbc03d3232de14b14f2ca
andreatrdt <75367731+andreatrdt@users.noreply.github.com>
2024-02-21 19:30:04 +0100

    Add files via upload

--- Assignment1/EuropeanOptionKIClosed.m
+++ Assignment1/EuropeanOptionKIClosed.m
@@ -20,10 +20,10 @@ else % if KI > K
     % Option is equivalent to a Vanilla call option of strike KI
     % plus a Cash-or-nothing call option of strike KI and cash amount KI-K
     d2 = @(F0,K,T,sigma) (log(F0/K) - 0.5*sigma^2*T)/(sigma*sqrt(T));
-    optionPrice = EuropeanOptionClosed(F0,KI,B,T,sigma, 1) + (KI-K) * normcdf(d2(F0,KI,T,sigma));
+    optionPrice = EuropeanOptionClosed(F0,KI,B,T,sigma, 1) + B*(KI-K) * normcdf(d2(F0,KI,T,sigma));
 end
 
 % discounting
-optionPrice = B * optionPrice;
+optionPrice = optionPrice;
 
 end
