03403209531ba8c013c2511db355ce2949274d69
NickToi <160607944+NicoloToia@users.noreply.github.com>
2024-02-22 12:02:49 +0100

    fix derivative
    Change derivative of the vega for chaah or notingh

--- Assignment1/VegaClosed.m
+++ Assignment1/VegaClosed.m
@@ -11,21 +11,20 @@ function vega = VegaClosed(F0,K,KI,B,T,sigma)
 %
 
 % set the change in volatility (1%)
-dSigma = 0.01;
 
 if (KI < K) 
     % the option is equivalent to a vanilla call of strike K
     d1 = (log(F0/K) + 0.5*sigma^2*T) / (sigma*sqrt(T));
-    vega = B * F0 * sqrt(T) * exp(-0.5*d1^2) / sqrt(2*pi) * dSigma;
+    vega = B * F0 * sqrt(T) * exp(-0.5*d1^2) / sqrt(2*pi);
 else 
     % compute the vega based on the composition of a vanilla with strike KI
     % and a money or nothing with strike KI and value (KI - K)
     d1 = (log(F0/KI) + 0.5*sigma^2*T) / (sigma*sqrt(T));
     d2 = d1 - sigma*sqrt(T);
     % Vanilla call with strike KI
-    vegaVanillaCall = B * F0 * sqrt(T) * exp(-0.5*d1^2) / sqrt(2*pi) * dSigma;
+    vegaVanillaCall = B * F0 * sqrt(T) * exp(-0.5*d1^2) / sqrt(2*pi);
     % Money or Nothing with strike K and value (KI - K)
-    vegaMoneyOrNothing = B * (KI - K) * exp(-0.5*d2^2) / sqrt(2*pi) * dSigma;
+    vegaMoneyOrNothing = B * (KI - K) * (exp(-0.5*d2^2) / sqrt(2*pi)) * ((-1/sigma^2) * (log(F0/KI)/sqrt(T)) - 0.5*sqrt(T)) ;
     % sum of the two vegas
     vega = vegaVanillaCall + vegaMoneyOrNothing;
 end
