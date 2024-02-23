2d0d1b306b9d59fc47a13b3780d2a52f163df353
jstringara <62561822+jstringara@users.noreply.github.com>
2024-02-23 14:13:59 +0100

    Main with bermudan and pseudo american

--- Assignment1/runAssign1_TBM.m
+++ Assignment1/runAssign1_TBM.m
@@ -218,8 +218,6 @@ fprintf('\nOPTION PRICE AV : %.4f \n',optionPriceAV);
 %   Price also -with the Tree- a Bermudan option, where the holder has also the right to exercise 
 %   the option at the end of every month, obtaining the stock at the strike price. 
 M = 1000;
-% S0 = K;  % TO BE CHECKED ---->   NON CREDO SERVANO
-% F0 = S0*exp(-d*TTM)/B;
 
 OptionPriceBermudan = BermudanOptionCRR(F0, K, B, TTM, sigma, M, flag);
 fprintf('\nCRRPriceBermudan      :   %.4f \n',OptionPriceBermudan);
@@ -230,26 +228,24 @@ fprintf('\nCRRPriceBermudan      :   %.4f \n',OptionPriceBermudan);
 %   with the corresponding European price. Discuss the results.
 
 d=0:0.005:0.06;
-Delta_price=zeros(length(d),1);
 OptionPriceBermudan = zeros(length(d),1);
+OptionPriceAM = zeros(length(d),1);
 OptionCRR = zeros(length(d),1);
 
 for i=1:length(d)
+    % recompute the forward price
     F0=S0*exp(-d(i)*TTM)/B;
-    OptionPriceBermudan(i) = BermudanOptionCRR(F0, K, B, TTM, sigma, M, flag);
+    [OptionPriceBermudan(i), OptionPriceAM(i)] = BermudanOptionCRR(F0, K, B, TTM, sigma, d(i), M);
     OptionCRR(i) = EuropeanOptionCRR(F0, K, B, TTM, sigma, M, flag);
-    Delta_price=OptionPriceBermudan-EuropeanOptionCRR(F0, K, B, TTM, sigma, M, flag);
-end
-
-if OptionPriceBermudan - OptionCRR >= 0
-    disp('true')
 end
 
 figure
 plot(d, OptionPriceBermudan, '-xb', 'LineWidth', 1); 
+hold on
+plot(d, OptionPriceAM, '-xg', 'LineWidth', 1); 
 hold on; 
 plot(d, OptionCRR, '-or', 'LineWidth', 1);
 xlabel('Dividend Yield'); 
 ylabel('Option Price'); 
 title('Comparison of Bermudan Option Price and European Option Price'); 
-legend('Bermudan Option Price', 'European Option Price'); 
+legend('Bermudan Option Price', 'Pseudo-American Option', 'European Option Price'); 
