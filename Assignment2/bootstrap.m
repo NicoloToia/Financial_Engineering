af55737a39b8c2184a5fd15348ae00c29a6f8253
jstringara <62561822+jstringara@users.noreply.github.com>
2024-02-26 18:36:56 +0100

    All mighty push, for thinking on our own

--- Assignment2/bootstrap.m
+++ Assignment2/bootstrap.m
@@ -9,45 +9,54 @@ function [dates, discounts]=bootstrap(datesSet, ratesSet)
 % dates: a vector of dates
 % discounts: a vector of discount factors
 
-depoDates = datesSet.depos(1:3); % first 3 deposit dates
+deposDates = datesSet.depos(1:3); % first 3 deposit dates
 futuresDates = datesSet.futures(1:7,:); % first 7 futures dates
-swapDates = datesSet.swaps(2:end); % all swap dates except the first one
+swapsDates = datesSet.swaps(2:end); % all swap dates except the first one
 
-depoRates = ratesSet.depos(1:3); % first 3 deposit rates
+deposRates = ratesSet.depos(1:3); % first 3 deposit rates
 futuresRates = ratesSet.futures(1:7,:); % first 7 futures rates
-swapRates = ratesSet.swaps(2:end); % all swap rates except the first one
+swapsRates = ratesSet.swaps(2:end); % all swap rates except the first one
 
 % take the mid of the ask and bid
-depoRates = 0.5 * (depoRates(:,1) + depoRates(:,2));
-futuresRates = 0.5 * (futuresRates(:,1) + futuresRates(:,2));
-swapRates = 0.5 * (swapRates(:,1) + swapRates(:,2));
+t0 = datesSet.settlement;
+deposRates = 0.5 * (deposRates(:,1) + deposRates(:,2));
+futuressRates = 0.5 * (futuresRates(:,1) + futuresRates(:,2));
+swapsRates = 0.5 * (swapsRates(:,1) + swapsRates(:,2));
 
 % create a vector of dates and discount factors
-dates = [depoDates; futuresDates(:, 2); swapDates];
-discounts = zeros(size(dates));
+dates = [t0];
+discounts = [1];
+zeroRates = [0];
 
-% deposit rates (use act/360 convention)
-depoDeltas = yearfrac(depoDates(1), depoDates(2), 2);
-discounts(1:3) = 1 ./ (1 + depoRates .* depoDeltas);
+% use the act/360 convention for deposit rates
+depoDelta = yearfrac(t0, deposDates, 2);
+dates = [dates, deposDates];
+discounts = [discounts, 1 ./ (1 + deposRates .* depoDelta)];
+zeroRates = [zeroRates, -log(discounts(2:end)) ./ depoDelta];
 
-% futures rates (use act/360 convention)
-for i=1:7
+% futures rates (use act/360)
+for i = 1:7
     % compute the forward discount factor
-
     delta = yearfrac(futuresDates(i, 1), futuresDates(i, 2), 2);
-    forwardDiscount = 1 / (1 + futuresRates(i) * delta)
-    % find the index of the previous and next dates
-    prevIdx = find(dates <= futuresDates(i, 1), 1, 'last')
-    nextIdx = find(dates >= futuresDates(i, 1), 1, 'first')
-    
-    % compute the discount factor for the settlement date
-    settlementDF = interpDF(futuresDates(i, 1), dates(prevIdx), dates(nextIdx), ...
-        discounts(prevIdx), discounts(nextIdx))
-
-    % compute the discount factor for the expiry date
-    discounts(i+length(depoRates)) = forwardDiscount * settlementDF;
-
+    forwardDiscount = 1 / (1 + futuresRates(i) * delta);
+    % compute the discount factor at the settlement date
+    settlementDF = interpDF(discounts,zeroRates, dates, futuresDates(i, 1));
+    % compute the discount factor at the expiry date
+    discounts(i+4) = forwardDiscount * settlementDF;
+    % update the zero rate
+    zeroRates(i+4) = -log(discounts(i+4)) / delta;
 end
+
+% % futures rates (use act/365 convention)
+% for i=1:7
+%     % compute the forward discount factor
+%     delta = yearfrac(futuresDates(i, 1), futuresDates(i, 2), 2);
+%     forwardDiscount = 1 / (1 + futuresRates(i) * delta);
+%     % compute the discount factor for the settlement date
+%     settlementDF = interpDF(dates, discounts, futuresDates(i, 2));
+%     % compute the discount factor for the expiry date
+%     discounts(i+length(depoRates)) = forwardDiscount * settlementDF;
+% end
 
 end
 
--- Assignment2/interpDF.m
+++ Assignment2/interpDF.m
@@ -1,12 +1,41 @@
-function [discount]=interpDF(targetDate, prevDate, nextDate, prevdisc ,nextdisc)
+function [discount]=interpDF(discounts,zeroRates, dates, targetDate)
 
-%   interpolation betwean two dates given a certain target date
-         
-if targetDate == prevDate
-     discount=prevdisc;
-else 
-    discount=interp1([prevDate,nextDate],[prevdisc,nextdisc],targetDate);
+% find the date that matches the target date
+idx = find(dates==targetDate);
+if ~isempty(idx)
+    discount = discounts(idx);
+    return;
 end
+
+% if there are dates that encapsulate the target date, interpolate
+
+
+
+
+
+
+
+
+
+
+% find the index of the dates that encapsulate the target date
+prevIdx = find(dates<=targetDate,1,'last');
+nextIdx = find(dates>targetDate,1,'first');
+prevDate = dates(prevIdx);
+nextDate = dates(nextIdx);
+
+% target dates coincides
+if targetDate == prevDate
+    y = zeroRates(prevIdx);
+else
+    prevRate = zeroRates(prevIdx)
+    nextRate = zeroRates(nextIdx)
+    y = prevRate + (nextRate - prevRate) / (nextDate - prevDate) * (targetDate - prevDate);
+end
+
+delta = yearfrac(dates(1),targetDate, 2);
+
+discount = exp(-y*delta);
 
 end
  
