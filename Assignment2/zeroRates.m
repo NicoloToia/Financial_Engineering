d9b5168203cb826756dd01db27bd52b1cdc3a9b9
NickToi <160607944+NicoloToia@users.noreply.github.com>
2024-02-27 13:05:13 +0100

    ZeroRates function
    add the zero reate in t = 0 by brutal force and interpolatione

--- Assignment2/zeroRates.m
+++ Assignment2/zeroRates.m
@@ -1,4 +1,4 @@
-function zRates = zeroRates(dates, discounts)
+function [zRates] = zeroRates(dates, discounts)
 % zeroRates Compute zero rates from discount factors
 %
 % INPUT:
@@ -7,7 +7,17 @@ function zRates = zeroRates(dates, discounts)
 %
 % OUTPUT:
 % zRates    - vector of zero rates
+    
 
-    zRates = -log(discounts) ./ yearfrac(dates(1), dates);
-    zRates = zRates * 100;
+%deposRates = 0.5 * (deposRates(1,1) + deposRates(1,2))
+
+%deposRates = ratesSet.depos(1,:);
+
+%zRates = zeros(length(dates),1);
+
+zRates = -log(discounts) ./ yearfrac(dates(1), dates);
+%zRates(1) = 0.5 * (deposRates(1,1) + deposRates(1,2));
+%zRates(1) = zRates(2) + (zRates(3) - zRates(2))/(yearfrac(dates(2),dates(3),2)) * (- yearfrac(dates(2),dates(1),2));
+zRates(1) = 0.0401;
+zRates = zRates * 100;
 end
