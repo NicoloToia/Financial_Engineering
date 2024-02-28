28957f27f0c6080fa4d39cfc72c03a8a6ab0aad4
jstringara <62561822+jstringara@users.noreply.github.com>
2024-02-28 11:14:26 +0100

    Delete unused file

--- Assignment2/find_couponPaymentDates.m
+++
@@ -1,7 +0,0 @@
-function [couponDates] = find_couponPaymentDates(datesSet, setDate)
-    %   coupon payments every 3 months for 6 years
-    couponDates=zeros(24,1);
-    for i=1:24
-        couponDates(i)=setDate+90;
-    end
-end
