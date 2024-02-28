ca1ba94bfbab24914eec898022f419b351640236
NickToi <160607944+NicoloToia@users.noreply.github.com>
2024-02-28 11:11:34 +0100

    fix  zero rates
    first rate equal to second

--- Assignment2/zeroRates.m
+++ Assignment2/zeroRates.m
@@ -8,16 +8,8 @@ function [zRates] = zeroRates(dates, discounts)
 % OUTPUT:
 % zRates    - vector of zero rates
     
-
-%deposRates = 0.5 * (deposRates(1,1) + deposRates(1,2))
-
-%deposRates = ratesSet.depos(1,:);
-
-%zRates = zeros(length(dates),1);
-
 zRates = -log(discounts) ./ yearfrac(dates(1), dates);
-%zRates(1) = 0.5 * (deposRates(1,1) + deposRates(1,2));
-%zRates(1) = zRates(2) + (zRates(3) - zRates(2))/(yearfrac(dates(2),dates(3),2)) * (- yearfrac(dates(2),dates(1),2));
-zRates(1) = 0.0401;
+zRates(1) = zRates(2);
 zRates = zRates * 100;
+
 end
