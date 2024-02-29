de214eda3fa70a6da3693c065656be427a2787f4
jstringara <62561822+jstringara@users.noreply.github.com>
2024-02-29 13:45:47 +0100

    Vectorialize

--- Assignment2/sensSwap.m
+++ Assignment2/sensSwap.m
@@ -18,13 +18,13 @@ deltas = [
     yearfrac(setDate, fixedLegPaymentDates(1));
     yearfrac(fixedLegPaymentDates(1:end-1), fixedLegPaymentDates(2:end));
 ];
-BPV = sum(deltas .* discountsFixedLeg) * bp;
+BPV = deltas' * discountsFixedLeg * bp;
 
 % original NPV
-NPV_0 = 1 - discountsFixedLeg(end) - fixedRate * sum(deltas .* discountsFixedLeg);
+NPV_0 = 1 - discountsFixedLeg(end) - fixedRate * deltas' * discountsFixedLeg;
 
 % compute the DV01
-NPV_shift = 1 - discountsFixedLeg_DV01(end) - fixedRate * sum(deltas .* discountsFixedLeg_DV01);
+NPV_shift = 1 - discountsFixedLeg_DV01(end) - fixedRate * deltas' * discountsFixedLeg_DV01;
 DV01 = NPV_shift - NPV_0;
 
 % parallel shift of zero rates
@@ -33,7 +33,7 @@ zeroRatesFixedLeg = zeroRatesFixedLeg(2:end); % remove the zero rate at the set 
 discountsFixedLeg_z = exp(-zeroRatesFixedLeg .* yearfrac(setDate, fixedLegPaymentDates));
 
 % compute the NPV with the shifted zero rates
-NPV_z = 1 - discountsFixedLeg_z(end) - fixedRate * sum(deltas .* discountsFixedLeg_z);
+NPV_z = 1 - discountsFixedLeg_z(end) - fixedRate * deltas' * discountsFixedLeg_z;
 DV01_z = NPV_z - NPV_0;
 
 end
