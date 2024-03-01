4ad30617be0874ae8855252d101ece0284e0f914
NickToi <160607944+NicoloToia@users.noreply.github.com>
2024-03-01 10:47:53 +0100

    comments

--- Assignment2/discountDepos.m
+++ Assignment2/discountDepos.m
@@ -1,20 +1,37 @@
-function [discounts, dates] = discountDepos(datesSet, ratesSet, DeposDate)
+function [discounts] = discountDepos(datesSet, ratesSet, discounts, DeposDate)
+% Bootstrap the discount factors for deposits
+%
+% INPUT
+% datesSet  : dates from market data stored as follow
+%              -> datesSet.settlement : Settlement Date
+%              -> datesSet.depos      : Vector of end calculation dates for Depos
+%              -> datesSet.futures    : Matrix with start/end dates for Futures
+%              -> datesSet.swaps      : Vector of end calculation date for Swaps
+% ratesSet  : market data for bid&ask rates stored as follow
+%              -> ratesSet.depos      : Matix bid&ask Depos
+%              -> ratesSet.futures    : Matix bid&ask Futures
+%              -> ratesSet.swaps      : Matix bid&ask Swaps
+% discounts : Inizialized vector for discounts of the length needed, 
+%             set as first value one
+% DeposDate : the function compute the discounts for the first n depos,
+%             where n = DeposDate
+%
+% OUTPUT
+% discounts : Discount Factors for depos
 
-% date notation 
+% Yearfrac Convention 
 ACT_360 = 2;
 
-% retrieve rates and dates
-deposDates = datesSet.depos(1:DeposDate); % first 3 deposit dates
-deposRates = ratesSet.depos(1:DeposDate,:); % first 3 deposit rates
-deposRates = 0.5 * (deposRates(:,1) + deposRates(:,2));
+% Retrieve rates and dates
+deposDates = datesSet.depos(1:DeposDate);               % first 3 depos dates
+deposRates = ratesSet.depos(1:DeposDate,:);             % first 3 depos rates
+deposRates = 0.5 * (deposRates(:,1) + deposRates(:,2)); % mid-market depos rates
 
-% settlement date and initialize output
+% Fix settlement date
 t0 = datesSet.settlement;
-dates = [t0];
-discounts = [1];
-% compute the discount factors
+
+% Compute the discount factors for depos
 depoDeltas = yearfrac(t0, deposDates, ACT_360);
-dates = [dates; deposDates];
-discounts = [discounts; 1 ./ (1 + deposRates .* depoDeltas)];
+discounts(2:DeposDate+1) = 1 ./ (1 + deposRates .* depoDeltas);
 
 end
