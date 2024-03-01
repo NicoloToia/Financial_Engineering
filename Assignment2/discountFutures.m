1a20fb6c6f8b15f38300fa23effa87f28cab00d5
NickToi <160607944+NicoloToia@users.noreply.github.com>
2024-03-01 10:48:03 +0100

    comments & new

--- Assignment2/discountFutures.m
+++ Assignment2/discountFutures.m
@@ -1,23 +1,42 @@
-function [discounts dates] = discountFutures(datesSet, ratesSet, discounts, dates, FutureDate)
+function [discounts] = discountFutures(datesSet, ratesSet, discounts, dates, FutureDate)
+% Bootstrap the discount factors for futures
+%
+% INPUT
+% datesSet   : dates from market data stored as follow
+%              -> datesSet.settlement : Settlement Date
+%              -> datesSet.depos      : Vector of end calculation dates for Depos
+%              -> datesSet.futures    : Matrix with start/end dates for Futures
+%              -> datesSet.swaps      : Vector of end calculation date for Swaps
+% ratesSet   : market data for bid&ask rates stored as follow
+%              -> ratesSet.depos      : Matix bid&ask Depos
+%              -> ratesSet.futures    : Matix bid&ask Futures
+%              -> ratesSet.swaps      : Matix bid&ask Swaps
+% discounts  : Inizialized vector for discounts of the length needed, 
+%             set as first value one
+% dates      : dates of discount factors curve bootstrap
+% FutureDate : the function compute the discounts for the first n futures,
+%              where n = FutureDate
+%
+% OUTPUT
+% discounts: Discount Factors for depos
     
-% date notation 
+% Yearfrac Convention 
 ACT_360 = 2;
-%   retrive data 
-futuresDates = datesSet.futures(1:FutureDate,:); % first 7 futures dates
-futuresRates = ratesSet.futures(1:FutureDate,:); % first 7 futures rates
-futuresRates = 0.5 * (futuresRates(:,1) + futuresRates(:,2));
 
-% compute rates from futures
+% Retrieve rates and dates
+futuresDates = datesSet.futures(1:FutureDate,:);              % first 7 futures dates
+futuresRates = ratesSet.futures(1:FutureDate,:);              % first 7 futures rates
+futuresRates = 0.5 * (futuresRates(:,1) + futuresRates(:,2)); % mid-market futures rates
+
+% Compute the discount factors for the futures
 for i = 1:FutureDate
     % compute the forward discount factor
     deltaForward = yearfrac(futuresDates(i, 1), futuresDates(i, 2), ACT_360);
     forwardDiscount = 1 / (1 + futuresRates(i) * deltaForward);
-    % compute the discount factor at the settlement date
-    settlementDF = intExtDF(discounts, dates, futuresDates(i, 1));
+    % find the discount factor at the settlement date by intExDF functions
+    settlementDF = intExtDF(discounts(1:3 + i), dates(1:3 + i), futuresDates(i, 1));
     % compute the discount factor at the expiry date
-    discounts = [discounts; forwardDiscount * settlementDF];
-    % update the zero rate
-    dates = [dates; futuresDates(i, 2)];
+    discounts(i+4) = forwardDiscount * settlementDF;                                    %% non SO COME METTERE INDICE CARINO
 end
 
 end
