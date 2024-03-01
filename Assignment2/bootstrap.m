ebb541e91589d81c1bdf3a0ccdfb9d1608296657
NickToi <160607944+NicoloToia@users.noreply.github.com>
2024-03-01 10:47:31 +0100

    new bootstrap & comments

--- Assignment2/bootstrap.m
+++ Assignment2/bootstrap.m
@@ -1,24 +1,41 @@
 function [dates, discounts]=bootstrap(datesSet, ratesSet)
 % Bootstrap the discount factors curve from the input dates and rates
 %
-% INPUTS:
-% datesSet: a vector of dates
-% ratesSet: a vector of rates
+% INPUT
+% datesSet: dates from market data stored as follow
+%            -> datesSet.settlement : Settlement Date
+%            -> datesSet.depos      : Vector of end calculation dates for Depos
+%            -> datesSet.futures    : Matrix with start/end dates for Futures
+%            -> datesSet.swaps      : Vector of end calculation date for Swaps
+% ratesSet: market data for bid&ask rates stored as follow
+%            -> ratesSet.depos      : Matix bid&ask Depos
+%            -> ratesSet.futures    : Matix bid&ask Futures
+%            -> ratesSet.swaps      : Matix bid&ask Swaps
 %
-% OUTPUTS:
-% dates: a vector of dates
-% discounts: a vector of discount factors
+% OUTPUT
+% dates    : Dates of discount factors curve bootstrap
+%            -> recall that the boostrap is considering the most liquid
+%            products, hence as default & "according" with the market the vector has the 
+%            following structure:
+%            [settelment date; first 3 depos expiries; first 7 futures expiries; form the 2nd swap expiry to end]
+% discounts: Discount Factors of the discount curve bootstrap at corresponding dates
 
-%   parameters fixed by trader
-DeposDate=3;    %   consider untill this Depo
-FutureDate=7;    %   consider untill this Future
-SwapStart=2;    %   consider ufrom this Swap
+% Parameters fixed by trader, based on liquidity
+DeposDate  = 3;     %   consider untill this Depos
+FutureDate = 7;     %   consider untill this Future
+SwapStart  = 2;     %   consider from this Swap
 
-% discount and dates for Depos
-[discounts dates] = discountDepos(datesSet, ratesSet, DeposDate);
-% discount and dates for Futures
-[discounts dates] = discountFutures(datesSet, ratesSet, discounts, dates, FutureDate);
-% discount and dates for Swaps
-[discounts dates] = discountSwaps(datesSet, ratesSet, discounts, dates , SwapStart);
+% Dates vector inizialization thanks to parameters above
+dates = [datesSet.settlement;datesSet.depos(1:DeposDate);datesSet.futures(1:FutureDate,2);...
+    datesSet.swaps(SwapStart:end)];
+% Inizialize discounts vector
+discounts = ones(length(dates),1);
+
+% discount for Depos
+[discounts] = discountDepos(datesSet, ratesSet, discounts, DeposDate);
+% discount for Futures
+[discounts] = discountFutures(datesSet, ratesSet, discounts, dates, FutureDate);
+% discount for Swaps
+[discounts] = discountSwaps(datesSet, ratesSet, discounts, dates , SwapStart);
 
 end
