1d381b9878a899a4fbefe7d1925dfa675b19f2d8
jstringara <62561822+jstringara@users.noreply.github.com>
2024-03-06 15:52:21 +0100

    Fix hazard curve to be more readable and correct

--- Assignment1_RM/PV_risky_bond_h.m
+++ Assignment1_RM/PV_risky_bond_h.m
@@ -4,7 +4,7 @@ l = length(cf_schedule);
 
 if l == 2 % one year case
     % save the coupon dates and values
-    couponDates = cf_schedule(:, 1)
+    couponDates = cf_schedule(:, 1);
     couponValues = cf_schedule(:, 2);
     % compute the discount factors
     ZC_dates = ZC_curve(:, 1);
@@ -19,8 +19,24 @@ if l == 2 % one year case
     survivalProb = [1; survivalProb];
     PV = couponValues' * B_bar + R * B' * (survivalProb(1:end-1) - survivalProb(2:end));
 elseif l == 4 % two year case
-    PV = 0;
-
+    % save the coupon dates and values
+    couponDates = cf_schedule(:, 1);
+    couponValues = cf_schedule(:, 2);
+    % compute the discount factors
+    ZC_dates = ZC_curve(:, 1);
+    ZC_rates = ZC_curve(:, 2);
+    ZC_curve = interp1(ZC_dates, ZC_rates, couponDates);
+    % compute the survival probabilities
+    h_curve = h_curve(:, 2);
+    survivalProb = zeros(4, 1);
+    survivalProb(1:2) = exp(-h_curve(1) * couponDates(1:2));
+    survivalProb(3:4) = exp(-h_curve(1) * couponDates(2)) * ...
+        exp(-h_curve(2) * (couponDates(3:4) - couponDates(2)));
+    B = exp(-ZC_curve .* couponDates);
+    B_bar = B .* survivalProb;
+    % add the survival probability of 1 at time 0
+    survivalProb = [1; survivalProb];
+    PV = couponValues' * B_bar + R * B' * (survivalProb(1:end-1) - survivalProb(2:end));
 else
     % throw error
     error('PV_risky_bond_h: invalid cash flow schedule');
--- Assignment1_RM/hazardCurve.m
+++ Assignment1_RM/hazardCurve.m
@@ -1,46 +1,58 @@
 function [h_curve] = hazardCurve(zeroCurve, R, dirtyPrice_1y, dirtyPrice_2y, ...
     couponSchedule_1y, couponSchedule_2y)
 
+% separate the coupons
+couponDates_1y = couponSchedule_1y(:,1);
+couponValues_1y = couponSchedule_1y(:,2);
+couponDates_2y = couponSchedule_2y(:,1);
+couponValues_2y = couponSchedule_2y(:,2);
+
 % get the zero curve
 zeroDates = zeroCurve(:,1);
 zeroRates = zeroCurve(:,2);
 % interpolate the dates not available in the zero curve
-zeroRates = interp1(zeroDates, zeroRates, couponSchedule_2y(:,1));
-discountFactors = exp(-zeroRates .* couponSchedule_2y(:,1));
+zeroRates_1y = interp1(zeroDates, zeroRates, couponDates_1y);
+zeroRates_2y = interp1(zeroDates, zeroRates, couponDates_2y);
+% compute discount factors
+B_1y = exp(-zeroRates_1y .* couponDates_1y);
+B_2y = exp(-zeroRates_2y .* couponDates_2y);
 
 % one year function of the hazard rate
-coupons_1y = couponSchedule_1y(:,2);
-yearfrac_1y = [0; couponSchedule_1y(:,1)];
-discountFactors_1y = discountFactors(1:2);
-discountCoupon_1y = discountFactors_1y .* coupons_1y;
-% price as a function of the hazard rate
-P_1y = @(h1) exp(-h1 * yearfrac_1y(2:3))' * discountCoupon_1y + ...
-    R * ( exp(-h1 * yearfrac_1y(1:end-1)) - exp(-h1 * yearfrac_1y(2:end)) )' * discountFactors_1y - ...
-    dirtyPrice_1y;
+function P_1y = Price_1y(h1)
+    survivalProb_1y = [1; exp(-h1 * couponDates_1y)];
+    B_bar_1y = B_1y .* exp(-h1 * couponDates_1y);
+    P_1y = couponValues_1y' * B_bar_1y + ...
+        R * B_1y' * (survivalProb_1y(1:end-1) - survivalProb_1y(2:end)) - ...
+        dirtyPrice_1y;
+end 
+
+P_1y = @(h1) Price_1y(h1);
+
 % compute the hazard rate
 h1 = fzero(P_1y, 0.01);
 
 % two year function of the hazard rate
-coupons_2y = couponSchedule_2y(:,2);
-yearfrac_2y = [0; couponSchedule_2y(:,1)];
-discountFactors_2y = discountFactors(1:4);
-discountCoupon_2y = discountFactors_2y .* coupons_2y;
-prevProb = exp(-h1 * yearfrac_2y(3));
-% price as a function of the hazard rate
-P_2y = @(h2) exp(-h1 * yearfrac_2y(2:3))' * discountCoupon_2y(1:2) + ...
-    prevProb * exp(-h2 * (yearfrac_2y(4:5) - yearfrac_2y(3)))' *  discountCoupon_2y(3:4) + ...
-    R * ( ...
-        ( exp(-h1 * yearfrac_2y(1:2)) - exp(-h1 * yearfrac_2y(2:3)) )' * discountFactors_2y(1:2) + ...
-        prevProb * (exp(-h2 * yearfrac_2y(4:5)) - exp(-h2 * yearfrac_2y(3)))' ...
-        * discountFactors_2y(3:4) ...
-    ) - dirtyPrice_2y;
+function P_2y = Price_2y(h2, h1)
+    % compute the survival probabilities
+    survivalProb = zeros(4,1);
+    survivalProb(1:2) = exp(-h1 * couponDates_2y(1:2));
+    survivalProb(3:4) = exp(-h2 * (couponDates_2y(3:4)-couponDates_2y(2))) * survivalProb(2);
+    % compute defaultable bond prices
+    B_bar_2y = B_2y .* survivalProb;
+    % compute the price
+    P_2y = couponValues_2y' * B_bar_2y + ...
+        R * B_2y' * ([1; survivalProb(1:end-1)] - survivalProb) - ...
+        dirtyPrice_2y;
+end 
+
+P_2y = @(h2) Price_2y(h2, h1);
 
 h2 = fzero(P_2y, 0.01);
 
 % return the hazard curve in term of basis points
 h_curve = [
-    yearfrac_1y(3), h1;
-    yearfrac_2y(5), h2;
+    couponSchedule_1y(end,1), h1;
+    couponSchedule_2y(end,1), h2
 ];
 
 end
