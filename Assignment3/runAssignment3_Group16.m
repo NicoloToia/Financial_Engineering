51e6ede00fdb1cd5c4f65b32960b8ee7e991830a
jstringara <62561822+jstringara@users.noreply.github.com>
2024-03-11 11:53:29 +0100

    Add old bootstrap data and new main file

---
+++ Assignment3/runAssignment3_Group16.m
@@ -0,0 +1,24 @@
+% runAssignment3_Group16
+%  group 16, AY2023-2024
+% Compute the 
+%
+% to run:
+% > runAssignment2_Group16
+
+% clear workspace
+clear all;
+close all;
+clc;
+
+% set the clock to find the time of execution
+tic;
+
+%% Settings
+
+formatData='dd/mm/yyyy'; % Pay attention to your computer settings 
+
+%% Load the data from the previous assignment
+
+load('discounts.mat');
+
+toc
