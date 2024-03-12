f09189429c0bf5af7555a84bb4f18da9e86cf645
jstringara <62561822+jstringara@users.noreply.github.com>
2024-03-12 14:17:45 +0100

    Fix updating of accrual sum

--- Assignment3/bootstrapExact.m
+++ Assignment3/bootstrapExact.m
@@ -7,25 +7,27 @@ intensities = zeros(length(spreadsCDS),1);
 % compute the survival probabilities and the intensities
 BPV_bar = 0; % sum of B(t0;t_i) * P(t_i) up to n-1
 sumE = 0; % sum of the e(t0;t_{i-1},t_i) up to n-1
-sumAccrual = 0; 
+sumAccrual = 0; % sum of delta_i * e(t0;t_{i-1},t_i) up to n-1
 
-for i = 1:length(spreadsCDS)
-    if i == 1
+for n = 1:length(spreadsCDS)
+    if n == 1
         prevProb = 1;
     else
-        prevProb = survProbs(i-1);
+        prevProb = survProbs(n-1);
     end
     % compute the numerator 
-    numerator = (1 - recovery) * (sumE + discountsCDS(i) * prevProb) - BPV_bar * spreadsCDS(i);
+    numerator = (1 - recovery) * (sumE + discountsCDS(n) * prevProb) - ...
+        spreadsCDS(n) * ( BPV_bar + sumAccrual + deltas(n) /2 * discountsCDS(n) * prevProb);
     % compute the denominator
-    denominator = spreadsCDS(i) * deltas(i) *  discountsCDS(i) + (1 - recovery) * discountsCDS(i);
+    denominator = spreadsCDS(n) * deltas(n) / 2 *  discountsCDS(n) + (1 - recovery) * discountsCDS(n);
     % compute the survival probability
-    survProbs(i) = numerator / denominator;
+    survProbs(n) = numerator / denominator;
     % compute the intensity
-    intensities(i) = -log(survProbs(i)/prevProb) / deltasIntensity(i);
+    intensities(n) = -log(survProbs(n)/prevProb) / deltasIntensity(n);
     % update the sums
-    BPV_bar = BPV_bar + deltas(i) * discounts(i) * survProbs(i);
-    sumE = sumE + discounts(i) * (prevProb - survProbs(i));
+    BPV_bar = BPV_bar + deltas(n) * discountsCDS(n) * survProbs(n);
+    sumE = sumE + discountsCDS(n) * (prevProb - survProbs(n));
+    sumAccrual = sumAccrual + deltas(n) / 2 * (prevProb - survProbs(n));
 end
 
 end
