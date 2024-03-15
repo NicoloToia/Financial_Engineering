b832131ba573801656959de3fb4a1508afa47038
NickToi <160607944+NicoloToia@users.noreply.github.com>
2024-03-15 01:44:56 +0100

    update_comment

--- Assignment3/PlotIntensities.m
+++ Assignment3/PlotIntensities.m
@@ -1,24 +1,37 @@
 function PlotIntensities(datesCDS, int_Approx, int_Exact, int_JT)
+% Plot of the Bootstrap results considering all the different flag case:
+% 1 approx; 2 exact; 3 Jarrow-Turnbull
+%
+% INPUT
+%   datesDF     : dates of the discount factors
+%   int_Approx  : intesities neglecting the accrual
+%   int_Exact   : intensities considering the accrual
+%   int_JT      : flat intensities Jarrow-Turmbull
+%
+% OUTPUT
+%   Plots of int_Approx vs int_Exact & qualitative plot int_JT
 
-    figure
-    stairs(datesCDS, int_Approx, '-')
-    hold on
-    stairs(datesCDS, int_Exact, '-')
-    legend('Approx', 'Exact')
-    datetick('x', 'mm/dd/yyyy', 'keepticks')
-    
-    % compute the cumulative mean of the intensities
-    cumIntensities = cumsum(int_Approx) ./ (1:length(int_Approx))';
+% Plot the intensities with and without accrual
+figure
+stairs(datesCDS, int_Approx, '-')
+hold on
+stairs(datesCDS, int_Exact, '-')
+legend('Approx', 'Exact')
+datetick('x', 'mm/dd/yyyy', 'keepticks')
 
-    % plot the intensities as a step function
-    figure
-    plot(datesCDS, cumIntensities, '-')
-    hold on
-    stairs(datesCDS, int_Approx, '-')
-    stairs(datesCDS, int_JT, '-')
-    legend('Mean', 'Approx', 'JT')
-    grid on
-    xlabel('Date')
-    ylabel('Intensity')
-    datetick('x', 'mm/dd/yyyy', 'keepticks')
+% Compute the cumulative mean of the intensities
+cumIntensities = cumsum(int_Approx) ./ (1:length(int_Approx))';
+
+% Plot of cumulative mean and JT intensities (qualitative), int_JT are flat
+figure
+plot(datesCDS, cumIntensities, '-')
+hold on
+stairs(datesCDS, int_Approx, '-')
+stairs(datesCDS, int_JT, '-')
+legend('Mean', 'Approx', 'JT')
+grid on
+xlabel('Date')
+ylabel('Intensity')
+datetick('x', 'mm/dd/yyyy', 'keepticks')
+
 end
