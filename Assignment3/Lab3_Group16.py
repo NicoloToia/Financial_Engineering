55907d9d65486dee40d59641f7241886b6736421
jstringara <62561822+jstringara@users.noreply.github.com>
2024-03-15 11:12:58 +0100

    Finalized python script

--- Assignment3/Lab3_Group16.py
+++ Assignment3/Lab3_Group16.py
@@ -1,3 +1,8 @@
+# silence the tensorflow warnings by setting `TF_ENABLE_ONEDNN_OPTS=0`
+import os
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
+# import the necessary libraries
 import pandas as pd
 import numpy as np
 from scipy.optimize import OptimizeResult
@@ -10,12 +15,16 @@ import statsmodels.api as sm
 from FE_Library import yearfrac
 from scipy.stats import norm
 
+## Exercise 1
+
 # Load Time Series
 quotes = pd.read_csv("DatasetPythonAss3.csv")
 quotes["Date"] = pd.to_datetime(quotes["Date"], format="%d/%m/%Y")
 quotes = quotes.set_index("Date")
 AAPL = quotes["AAPL"]
 SPX = quotes["SPX"]
+
+## Exercise 2
 
 # Plot Time Series
 plt.figure(1)
@@ -56,7 +65,9 @@ plt.xlabel('Datas')
 plt.ylabel('Prices')
 plt.show()
 
-# Regressions
+## Exercise 3
+
+# Regression
 model = LinearRegression()
 model.fit(logReturnSPX.values.reshape(-1, 1), logReturnAAPL.values.reshape(-1, 1))
 
@@ -69,57 +80,61 @@ plt.legend()
 plt.show()
 
 slope = model.coef_[0]
-print ("The slope is equal to", slope)
+print (f"The slope of the linear regression is {slope[0]:.4f}")
+
+## Exercise 4
+
 # YearFrac
 first_element = quotes.index[0]
 last_element = quotes.index[-1]
 
-print("The first element is", first_element, "and the last one is", last_element)
 fractionYear = yearfrac(first_element, last_element, 3)
-print("The year frac between the first and the last element is",fractionYear)
+print(f"The fraction of year between the first and the last element is {fractionYear:.4f}")
+
+## Exercise 5
 
 # Interpolate
-x = np.array([0, 1, 2, 3, 4]).reshape(-1, 1)
-f = np.array([1, 2, 3.5, 4, 2])
-model = LinearRegression()
-model.fit(x, f)
-point = np.array([[2.7]])
-interp_val = model.predict(point)
-print("The linear interpolation in", point, "is equal to",interp_val)
+fx = [1, 2, 3.5, 4, 2]
+x = [0, 1, 2, 3, 4]
+
+# linearn interpolation
+x_interp = 2.7
+y_interp = np.interp(x_interp, x, fx)
+
+print(f"The linear interpolation of {x_interp} is {y_interp:.4f}")
+
+## Exercise 6
+
 # Simulation of a standard normal random variable
 np.random.seed(42)
-standard_normal_samples = np.random.randn(100)
-plt.hist(standard_normal_samples, bins=40, density=True, alpha=0.7, color='blue')
-plt.title('Histogram of Standard Normal Distribution')
-plt.xlabel('Value')
-plt.ylabel('Frequency')
-plt.show()
 
 # show that the variance converges to 1 as the number of simulations increases
 n = np.linspace(100, 10000, 100)
-N = len(n)
-variance = np.zeros(N)
-for i in range(N):
-    standard_normal_samples = np.random.randn(int(n[i]))
-    variance[i] = np.var(standard_normal_samples)
-plt.plot(n, variance)
+variances = [
+    np.var(np.random.randn(int(i)))
+    for i in n
+]
+
+plt.plot(n, variances)
 plt.title('Variance of simulated standard gaussian')
 plt.axhline(y=1, color='red', linestyle='--')
 plt.xlabel('N')
 plt.ylabel('Variance')
 plt.show()
 
-quantile_0_9_simulated = np.percentile(standard_normal_samples, 90)
-quantile_0_9_real = norm.ppf(0.9)
-CDF_at_quantile_0_9_real = norm.cdf(quantile_0_9_real)
-CDF_at_quantile_0_9_simulated = norm.cdf(quantile_0_9_simulated)
+# show that the gaussian CDF of the simulated data gives the correct result
+z_sample = np.random.randn(10000)
+quantile_90 = np.percentile(z_sample, 90)
+CDF_sample = norm.cdf(quantile_90)
 
-print("The CDF evaluated with the real quantile 0.9 is", CDF_at_quantile_0_9_real)
-print("The CDF evaluated with the simulated quantile 0.9 is", CDF_at_quantile_0_9_simulated)
-print("Hence the absolute value of the difference between the two CDF is", abs(CDF_at_quantile_0_9_real-CDF_at_quantile_0_9_simulated))
+# print the result
+print(f"The CDF evaluated with the simulated quantile 0.9 is {CDF_sample:.4f}")
 
-# Minimization
-def f(x):
-    return (x[0]-3)**2+(x[1]-7)**2
-x_y_min = sc.optimize.minimize(f,(0, 0))
-print("The minimum of the function f is", x_y_min)
+## Exercise 7
+
+# minimize the function (x-3)^2 + (y-7)^2
+minimization = sc.optimize.minimize(lambda x: (x[0]-3)**2 + (x[1]-7)**2, [0, 0])
+
+# print the coordinates
+print(f"The minimum is found in ({minimization.x[0]:.4f}, {minimization.x[1]:.4f})")
+
