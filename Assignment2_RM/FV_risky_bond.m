325d6cdce0cecffdc7b8bf275e27f87e2edc1391
jstringara <62561822+jstringara@users.noreply.github.com>
2024-03-21 10:50:20 +0100

    Fix risky bond to not include one year coupon

--- Assignment2_RM/FV_risky_bond.m
+++ Assignment2_RM/FV_risky_bond.m
@@ -19,11 +19,12 @@ zeroRates = interp1(ZC_curve(:,1), ZC_curve(:,2), dates, 'linear');
 DF = exp(-zeroRates .* dates);
 
 % compute the forward discount factors in one year
-future_DF = DF(2:end) / DF(2);
+future_DF = DF(3:end) / DF(2);
+size(future_DF)
 
 % get the future coupons and dates
-future_coupons = coupons(2:end);
-future_dates = dates(2:end);
+future_coupons = coupons(3:end);
+future_dates = dates(3:end);
 
 % get the hazard rates from the Q matrix
 hazard_rates = -log(1 - Q(:, end));
@@ -31,12 +32,12 @@ hazard_rates = -log(1 - Q(:, end));
 % IG
 Prob_IG = exp( -hazard_rates(1) * (future_dates - dates(2) ) );
 B_bar_IG = future_DF .* Prob_IG;
-FV(1) = future_coupons' * B_bar_IG + Recovery * 100 * future_DF(2:end)' * (Prob_IG(1:end-1) - Prob_IG(2:end));
+FV(1) = future_coupons' * B_bar_IG + Recovery * 100 * future_DF' * ([1;Prob_IG(1:end-1)] - Prob_IG);
 
 % HY
 Prob_HY = exp( -hazard_rates(2) * (future_dates - dates(2) ) );
 B_bar_HY = future_DF .* Prob_HY;
-FV(2) = future_coupons' * B_bar_HY + Recovery * 100 * future_DF(2:end)' * (Prob_HY(1:end-1) - Prob_HY(2:end));
+FV(2) = future_coupons' * B_bar_HY + Recovery * 100 * future_DF' * (Prob_HY(1:end-1) - Prob_HY);
 
 % Default
 FV(3) =  Recovery * 100;
