7e4c7b0061308921c6ebb249610c8e6ae7698b7b
jstringara <62561822+jstringara@users.noreply.github.com>
2024-03-21 17:14:11 +0100

    Move stuff to functions

--- Assignment4/Assignment4_lib.py
+++ Assignment4/Assignment4_lib.py
@@ -28,3 +28,42 @@ def HSMeasurements(returns, alpha, weights, portfolioValue, riskMeasureTimeInter
     ES = losses.iloc[:n_quantile].mean() * np.sqrt(riskMeasureTimeIntervalInDay)
 
     return ES, VaR
+
+def bootstrapStatistical(numberOfSamplesToBootstrap, returns):
+    """
+        This function computes the bootstrap samples of the returns.
+        
+        Args:
+        - numberOfSamplesToBootstrap: the number of samples to draw from the returns (integer)
+        - returns: a pandas dataframe of asset returns (n, m)
+    """
+
+    return returns.sample(n=numberOfSamplesToBootstrap, replace=True)
+
+def plausibilityCheck(returns, portfolioWeights, alpha, portfolioValue, riskMeasureTimeIntervalInDay):
+
+    """
+        This function computes the plausibility check for the VaR of a portfolio of assets.
+
+        Args:
+        - returns: a pandas dataframe of asset returns (n, m)
+        - portfolioWeights: a numpy array of portfolio weights (m, )
+        - alpha: the confidence level (scalar)
+        - portfolioValue: the value of the whole portfolio (scalar)
+        - riskMeasureTimeIntervalInDay: the time interval for risk measure expressed in days (scalar)
+    """
+
+    # extract the two quantiles
+    lb = returns.quantile(1-alpha)
+    ub = returns.quantile(alpha)
+
+    # compute the signed VaR
+    sVaR = portfolioValue * portfolioWeights * (abs(lb) + abs(ub)) / 2
+
+    # correlation matrix
+    corr = returns.corr()
+
+    # compute the VaR with the thumb rule
+    VaR = np.sqrt(riskMeasureTimeIntervalInDay) * np.sqrt(sVaR @ corr @ sVaR)
+
+    return VaR
