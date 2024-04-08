bd69d8af57b2e986cc726780d0bee4177a8d49ef
jstringara <62561822+jstringara@users.noreply.github.com>
2024-04-08 14:47:38 +0200

    runAssignment5.m

---
+++ Assignment5/runAssignment5.m
@@ -0,0 +1,35 @@
+% runAssignment5_Group16
+%  group 16, AY2023-2024
+% Compute the 
+%
+% to run:
+% > runAssignment2_Group16
+
+% clear workspace
+clear all;
+close all;
+clc;
+
+% NOTE : for long computational time the number of iterations are set by
+% default at 1e4, use a larger number if more precision is required
+
+%% Settings
+
+formatData ='dd/MM/yyyy'; % Pay attention to your computer settings 
+
+rng(42);   % Fix the random number generator ("the answer to Life, the Universe, and Everything")
+
+tic;       % Set the clock to find the time of execution
+
+%% Point 1: Certificate Pricing
+
+
+%% Point 2: Pricing Digital Option
+
+
+%% Point 3: Pricing
+
+
+%% Point 4: Volatility Surface Calibration
+
+toc
