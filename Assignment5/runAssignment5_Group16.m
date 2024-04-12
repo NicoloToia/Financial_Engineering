2f961a924f83c776bb705154fc5027bdefc26a93
jstringara <62561822+jstringara@users.noreply.github.com>
2024-04-12 17:03:07 +0200

    Refactor certificate into its own function

--- Assignment5/runAssignment5_Group16.m
+++ Assignment5/runAssignment5_Group16.m
@@ -3,15 +3,12 @@
 % Compute the 
 %
 % to run:
-% > runAssignment2_Group16
+% > runAssignment5_Group16
 
 % clear workspace
 clear all;
 close all;
 clc;
-
-% NOTE : for long computational time the number of iterations are set by
-% default at 1e4, use a larger number if more precision is required
 
 %% Settings
 
@@ -46,96 +43,39 @@ t0 = datetime(2008, 2, 18, 'Format', formatData);
 ENEL_0 = 100;
 AXA_0 = 200;
 % volatilities
-sigma_ENEL = 16.1 / 100;
-sigma_AXA = 20 / 100;
-rho = 40 / 100;
+sigma_ENEL = 16.1 / 100; % 16.1%
+sigma_AXA = 20 / 100; % 20%
+rho = 40 / 100; % 40%
 % dividend yields
-d_ENEL = 2.5 / 100;
-d_AXA = 2.7 / 100;
+d_ENEL = 2.5 / 100; % 2.5%
+d_AXA = 2.7 / 100; % 2.7%
 
 %% Find the dates of the cash flows
 
-% fixed leg (annualy for 4 years)
-fixedLegDates = t0 + calyears(0:4);
-% move to business days if needed (follow convention, no holidays)
-fixedLegDates(~isbusday(fixedLegDates,0)) = busdate(fixedLegDates(~isbusday(fixedLegDates,0)), "follow", 0);
+% party B dates (annualy for 4 years), 4 dates
+partyB_dates = t0 + calyears(1:4);
+% move to business days if needed (modified follow convention, no holidays)
+partyB_dates(~isbusday(partyB_dates,0)) = busdate(partyB_dates(~isbusday(partyB_dates,0)), "modifiedfollow", 0);
+disp('Party B dates: ');
+disp(partyB_dates);
 % convert to datenums
-fixedLegDates = datenum(fixedLegDates);
+partyB_dates = datenum(partyB_dates);
 
-% floating leg (quarterly for 4 years)
-floatingLegDates = t0 + calmonths(0:3:48);
-floatingLegDates(~isbusday(floatingLegDates,0)) = busdate(floatingLegDates(~isbusday(floatingLegDates,0)), "follow", 0);
-floatingLegDates = datenum(floatingLegDates);
-
-%% Simulate underlying prices and compute the party B leg
-
-% compute the year fraction between fixedLegDates
-ACT_365 = 3;
-deltas_fixed = yearfrac(fixedLegDates(1:end-1), fixedLegDates(2:end), ACT_365);
-
-% compute the discount factors for the fixed leg
-discounts_fixed = intExtDF(discounts, dates, fixedLegDates);
-% compute the forward discount factors
-forward_discounts_fixed = discounts_fixed(2:end) ./ discounts_fixed(1:end-1);
-
-% number of simulations
-N = 1e7;
-
-% variables to store the simulations
-ENEL = zeros(N, 5);
-AXA = zeros(N, 5);
-% set initial values
-ENEL(:,1) = ENEL_0;
-AXA(:,1) = AXA_0;
-
-% for each time step
-for i = 1:4
-    % extract the two gaussian random variables
-    Z = mvnrnd([0 0], [1 rho; rho 1], N);
-    Z_ENEL = Z(:, 1);
-    Z_AXA = Z(:, 2);
-    % update the prices
-    ENEL(:, i+1) = 1/forward_discounts_fixed(i) * ENEL(:, i) .* exp((-d_ENEL - 0.5 * sigma_ENEL^2) * deltas_fixed(i) + sigma_ENEL * sqrt(deltas_fixed(i)) * Z_ENEL);
-    AXA(:, i+1) = 1/forward_discounts_fixed(i) * AXA(:, i) .* exp((-d_AXA - 0.5 * sigma_AXA^2) * deltas_fixed(i) + sigma_AXA * sqrt(deltas_fixed(i)) * Z_AXA);
-end 
-
-% compute the returns each year
-ratio_ENEL = ENEL(:, 2:end) ./ ENEL(:, 1:end-1);
-ratio_AXA = AXA(:, 2:end) ./ AXA(:, 1:end-1);
-
-% compute the final coupon
-S_t = 1/4 * sum(1/2 * ratio_ENEL + 1/2 * ratio_AXA, 2);
-
-% compute the discounted payoffs
-disc_payoff = discounts_fixed(end) * max(S_t - P, 0) * principal_Amount;
-% compute the mean
-maturity_payment_B = mean(disc_payoff);
-% initial party B payment
-start_payment_B = X * principal_Amount;
-
-%% Party A leg
-
-% compute discount factors for the floating leg
-discounts_floating = intExtDF(discounts, dates, floatingLegDates);
-
-% compute the deltas
-ACT_360 = 2;
-deltas_floating = yearfrac(floatingLegDates(1:end-1), floatingLegDates(2:end), ACT_360);
-
-% compute the s_spol payments
-NPV_s_spol = s_spol * sum(deltas_floating .* discounts_floating(2:end)) * principal_Amount;
-
-% compute the Libor payments
-NPV_libor = (1 - discounts_fixed(end)) * principal_Amount;
-
-% payment at maturity
-maturity_payment_A = (1-P) * discounts_fixed(end) * principal_Amount;
-
-party_A_leg = NPV_libor + NPV_s_spol + maturity_payment_A;
+% party A dates (quarterly for 4 years) 12 dates
+partyA_dates = t0 + calmonths(3:3:48);
+partyA_dates(~isbusday(partyA_dates,0)) = busdate(partyA_dates(~isbusday(partyA_dates,0)), "follow", 0);
+disp('Party A dates: ');
+disp(partyA_dates);
+partyA_dates = datenum(partyA_dates);
 
 %% Compute the partecipation coefficient
 
-alpha = (party_A_leg - start_payment_B) / maturity_payment_B
+N_sim = 1e7;
+
+alpha = priceCertificate(ENEL_0,  sigma_ENEL, d_ENEL, AXA_0, sigma_AXA, d_AXA, rho, s_spol, P, X, ...
+    principal_Amount, N_sim, partyA_dates, partyB_dates, dates, discounts);
+
+disp(['The participation coefficient is: ', num2str(alpha)]);
 
 %% Point 2: Pricing Digital Option
 
@@ -144,6 +84,7 @@ Notional = 1e7;
 % strike
 k = cSelect.reference;
 % maturity
+ACT_365 = 3;
 T = yearfrac(t0, t0 + calyears(1), ACT_365);
 % value of the digital option at maturity if s > k
 payment = 0.05 * Notional;
