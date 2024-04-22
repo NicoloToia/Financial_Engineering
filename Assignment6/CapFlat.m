5144564a0ed2763f71413d69e8e12feb1011788c
jstringara <62561822+jstringara@users.noreply.github.com>
2024-04-22 18:35:11 +0200

    Caplet initial commit

---
+++ Assignment6/CapFlat.m
@@ -0,0 +1,42 @@
+function Price = CapFlat(Strike, Vol, startDate, paymentDates, skipFirst, discounts, dates)
+% CapFlat: Compute the price of a cap using the flat volatility assumption
+%
+% INPUT
+%   Strike : Strike price of the cap
+%   Vol : Volatility of the cap
+%   paymentDates : Payment dates of the cap (do not include the settlement date)
+%   skipFirst : Skip the first payment date
+%   discounts : Discount factors
+%   dates : Dates of the discount factors
+
+% compute the deltas
+ACT_360 = 2;
+deltas = yearfrac([startDate;paymentDates(1:end-1)], paymentDates, ACT_360);
+
+% compute the discount factors
+DF = intExtDF(discounts, dates, [startDate;paymentDates]); % this is one more than the deltas
+
+% compute the forward rates
+fwdDiscounts = DF(2:end) ./ DF(1:end-1);
+% compute the forward Libor
+% the ith represents the forward Libor from i to i+1
+fwdLibor = 1 ./ deltas .* (1 ./ fwdDiscounts - 1);
+
+% transform the skipFirst from boolean to integer
+if skipFirst
+    skipFirst = 1;
+else
+    skipFirst = 0;
+end
+
+% compute the caplets prices using the Bachelier formula
+Caplets = zeros(length(paymentDates), 1);
+
+% price the first Caplet
+for i = 1+skipFirst:length(paymentDates)
+    Caplets(i) = CapletBachelier(fwdLibor(i), (1+Strike/100)*fwdLibor(i), Vol, deltas(i), paymentDates(i), dates(1), DF(i+1));
+end
+
+Price = sum(Caplets);
+
+end
--- Assignment6/capPrices.m
+++ Assignment6/capPrices.m
@@ -8,15 +8,7 @@ CapPrices = zeros(length(ttms), length(strikes));
 first_year_payment_dates = datetime(dates(1), 'ConvertFrom', 'datenum') + calmonths(3:3:12)';
 first_year_payment_dates = datenum(first_year_payment_dates);
 for i = 1:length(strikes)
-    CapPrices(1,i) = Cap3mFlat(strikes(i), Vols(1, i), first_year_payment_dates, dates(1), true, discounts, dates);
-end
-
-CapPrices
-
-% price the caps
-for i = 2:length(ttms)
-    for j = 1:length(strikes)
-    end
+    CapPrices(1,i) = CapFlat(strikes(i), Vols(1, i), dates(1), first_year_payment_dates, true, discounts, dates);
 end
     
 end
