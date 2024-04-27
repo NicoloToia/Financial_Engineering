cb948ce3e5155a0291cfdb2101bbb99c39ed43fb
jstringara <62561822+jstringara@users.noreply.github.com>
2024-04-27 14:46:06 +0200

    Modify plot for future use

--- Assignment6/Plotting/plot_delta_buckets.m
+++ Assignment6/Plotting/plot_delta_buckets.m
@@ -6,11 +6,16 @@ function plot_delta_buckets(dates, buckets)
 %   buckets: delta-bucket sensitivities
 
 figure;
-plot(dates, buckets, 'LineWidth', 2);
+% plot the points (filled circles) and connect them with a line
+plot(dates, buckets, 'LineWidth', 2, 'Marker', 'o', 'MarkerSize', 5);
 title('Delta-Bucket Sensitivities');
 xlabel('Dates');
 ylabel('Delta-Bucket Sensitivities');
+% show all dates rotated by 45 degrees
 datetick('x', 'mm/dd/yyyy', 'keeplimits', 'keepticks');
+% date_ticks = datestr(dates, 'dd-mmm-yyyy');
+% set(gca, 'XTick', dates, 'XTickLabel', date_ticks, 'XTickLabelRotation', 45);
+
 grid on;
 
 end
