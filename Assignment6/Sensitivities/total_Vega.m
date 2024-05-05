0f2f0dc18238d4112d5ed2bd98e0e24e75a42834
jstringara <62561822+jstringara@users.noreply.github.com>
2024-05-05 12:56:25 +0200

    Blazingly fast total vega

--- Assignment6/Sensitivities/total_Vega.m
+++ Assignment6/Sensitivities/total_Vega.m
@@ -1,5 +1,5 @@
 function Vega = total_Vega(mkt_vols, ttms, strikes, X_0, spol_A, fixed_rate_B, spol_B, ...
-    cap_5y, cap_10y, cap_15y, discounts, dates)
+    cap_5y, cap_10y, cap_15y, caplet_ttms, caplet_yf, caplet_DF, Libor)
 % TOTAL_VEGA computes the total vega of the certificate
 %
 % INPUTS
@@ -12,22 +12,30 @@ function Vega = total_Vega(mkt_vols, ttms, strikes, X_0, spol_A, fixed_rate_B, s
 %   cap_5y: strike of the cap from 0 to 5y
 %   cap_10y: strike of the cap from 5 to 10y
 %   cap_15y: strike of the cap from 10 to 15y
-%   discounts: discounts
-%   dates: dates of the market data
+%   caplet_ttms: caplet maturities
+%   caplet_yf: caplet year fractions
+%   caplet_DF: caplet discount factors
+%   Libor: forward Libor rates
 
-    % shock the volatility of the caps by 1bp
-    shift = 10^(-4);
-    % shift the market vols by 1bp
-    shift_vols = mkt_vols + shift;
+% shock the volatility of the caps by 1bp
+shift = 10^(-4);
+% shift the market vols by 1bp
+shift_vols = mkt_vols + shift;
 
-    % recompute the market prices
-    mkt_prices = MarketCapPrices(ttms, strikes, shift_vols, discounts, dates);
+% recompute the market prices
+mkt_prices = MarketCapPrices(ttms, strikes, shift_vols, caplet_ttms, caplet_yf, caplet_DF, Libor);
 
-    % recalibrate the spot volatilities
-    [spot_ttms, spot_vols] = spotVols(mkt_prices, ttms, strikes, shift_vols, discounts, dates);
-    % recalculate the upfront
-    X_1 = computeUpfront(spot_vols, spot_ttms, strikes, dates(1), spol_A, fixed_rate_B, spol_B, ...
-        cap_5y, cap_10y, cap_15y, discounts, dates);
-    % compute the total vega
-    Vega = (X_1 - X_0);
+% recalibrate the spot volatilities
+spot_vols = spotVols(mkt_prices, ttms, strikes, shift_vols, caplet_ttms, caplet_yf, caplet_DF, Libor);
+% select only the needed data for the certificate
+cf_caplet_ttms = caplet_ttms(1:15*4);
+cf_caplet_yf = caplet_yf(1:15*4);
+cf_caplet_DF = caplet_DF(1:15*4);
+cf_libor = Libor(1:15*4);
+% recalculate the upfront
+X_1 = computeUpfront(spot_vols, caplet_ttms, strikes, spol_A, fixed_rate_B, spol_B, ...
+    cap_5y, cap_10y, cap_15y, cf_caplet_ttms, cf_caplet_yf, cf_caplet_DF, cf_libor);
+% compute the total vega
+Vega = (X_1 - X_0);
+
 end
--- Assignment6/runAssignment6_Group16.m
+++ Assignment6/runAssignment6_Group16.m
@@ -155,7 +155,7 @@ disp('--- --- ---')
 %% Total Vega
 
 total_vega = total_Vega(mkt_vols, ttms, strikes, X, spol_A, fixed_rate_B, spol_B, ...
-    cap_rate_5y, cap_rate_10y, cap_rate_15y, discounts, dates);
+    cap_rate_5y, cap_rate_10y, cap_rate_15y, caplet_ttms, caplet_yf, caplet_DF, fwd_Libor);
 % print the total vega
 disp('--- Total vega of the Certificate ---')
 disp(['The total vega is: ', num2str(total_vega * 100), ' (Notional = 100) EUR']);
@@ -186,7 +186,7 @@ vega_dates = datenum(vega_dates);
 
 % compute the vega bucket sensitivities
 vega_buckets = vegaBuckets(mkt_vols, ttms, strikes, X, spol_A, fixed_rate_B, spol_B, ...
-    cap_rate_5y, cap_rate_10y, cap_rate_15y, discounts, dates);
+    cap_rate_5y, cap_rate_10y, cap_rate_15y, 
 
 disp('--- Vega-bucket sensitivity of the Certificate ---')
 disp('Date | Vega (Notional = 100) | Vega (EUR) |')
