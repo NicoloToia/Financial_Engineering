e3b684f242fdcaaf8272a19c8733bec39d95ed42
AliVailati <alicev2000@gmail.com>
2024-05-06 11:39:45 +0200

    dati es 1

--- Assignment7/runAssignment7_Group16.m
+++ Assignment7/runAssignment7_Group16.m
@@ -12,3 +12,20 @@ close all
 % fix random seed
 rng(42) 
 
+%% Load data for exercise 1
+principal = 100*1e9; % 100 billion
+settlement_date = datenum(datetime(2008, 2, 19)); % 19-Feb-2008
+maturity = 2; % 2 years
+ACT_30_360 = 2; % 30/360 day count convention   
+coupon_1y = 0.06; 
+coupon_2y = 0.02; 
+strike = 3200; 
+trigger = 0.06;
+
+%coupon dates each year
+couponDates = datetime(settlement_date, 'ConvertFrom', 'datenum') + calyears(1:2)';
+couponDates(~isbusday(couponDates, eurCalendar())) = busdate(couponDates(~isbusday(couponDates, eurCalendar())), 'modifiedfollow', eurCalendar());
+couponDates = datenum(couponDates);
+
+
+
