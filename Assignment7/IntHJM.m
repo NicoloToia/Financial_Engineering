2e83bd6c1b8a2cf8ac4291f0631bce37481df5f8
NickToi <160607944+NicoloToia@users.noreply.github.com>
2024-05-09 15:06:00 +0200

    gg

--- Assignment7/IntHJM.m
+++ Assignment7/IntHJM.m
@@ -1,5 +1,11 @@
-function IntHJM = IntHJM(a, sigma, t_i, t0)
+function IntHJM = IntHJM(a, sigma, t_i, t0, tau)
 
-IntHJM = (sigma/a)^2 * (t_i - t0 - 3/(2*a) + 1/a * exp(-a*(t_i-t0)) * (2 - 0.5*exp(-a*(t_i -t0))) );
+% define the integrand function
+integrand = @(u) ( (sigmaHJM(a, sigma, (t_i + tau), u)).^2 - (sigmaHJM(a, sigma, t_i, u)).^2 );
+
+% compute the integral
+IntHJM = integral(integrand, t0, t_i);
+
+% IntHJM = (sigma/a)^2 * (t_i - t0 - 3/(2*a) + 1/a * exp(-a*(t_i-t0)) * (2 - 0.5*exp(-a*(t_i -t0))) );
 
 end
